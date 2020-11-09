eb55b1834794d44a69ff265417993d05a375ba26
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-10 01:11:59 +0300

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  FooterWrapper,
+  FooterScialWrapper,
+  FooterSocialIcons,
+} from "../elements"
+
+export const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      facebook: file(relativePath: { eq: "facebook.svg" }) {
+        publicURL
+      }
+      linkedin: file(relativePath: { eq: "linkedin.svg" }) {
+        publicURL
+      }
+      instagram: file(relativePath: { eq: "instagram.svg" }) {
+        publicURL
+      }
+      twitter: file(relativePath: { eq: "twitter.svg" }) {
+        publicURL
+      }
+    }
+  `)
+  return (
+    <FooterWrapper>
+      <FooterScialWrapper>
+        <FooterSocialIcons>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.facebook.publicURL} alt="Facebook Logo" />
+          </a>
+
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.twitter.publicURL} alt="Twitter Logo" />
+          </a>
+
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.linkedin.publicURL} alt="Linkedin Logo" />
+          </a>
+
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.instagram.publicURL} alt="Instagram Logo" />
+          </a>
+        </FooterSocialIcons>
+        <p>
+          2020 © Dursun Eryılmaz. All rigths reserved.
+        </p>
+      </FooterScialWrapper>
+    </FooterWrapper>
+  )
+}
